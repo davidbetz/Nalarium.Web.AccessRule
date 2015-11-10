0389795096b48127c51ab730033b2d80c93a23a2
David Betz <dfb@davidbetz.net>
2015-11-09 23:51:11 -0600

    move

---
+++ Nalarium.Web.AccessRule/_ActionExecutor/WriteActionExecutor.cs
@@ -0,0 +1,20 @@
+﻿#region Copyright
+
+//+ Nalarium Pro 3.0 - Web Module
+//+ Copyright © Jampad Technology, Inc. 2008-2010
+
+#endregion
+
+//+
+namespace Nalarium.Web.AccessRule
+{
+    public class WriteActionExecutor : ActionExecutor
+    {
+        //- @Execute -//
+        public override void Execute()
+        {
+            Nalarium.Web.Http.Response.Write(Value);
+            Nalarium.Web.Http.Response.End();
+        }
+    }
+}
